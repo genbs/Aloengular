e008879f5090c6ba8e6362b001c200f9f04843d9
gennaro <gennaro.bs@gmail.com>
2016-01-20 21:37:31 +0100

    0.0.0

---
+++ aloengular.js
@@ -0,0 +1,19 @@
+/**
+ * Aloengular
+ * created at 01-20-2016
+ * author Gennaro Bosone <gennaro.bs@gmail.com>
+ *
+ */
+(function(){
+
+	'use strict';
+
+	angular
+		.module('aloengular',[
+			'aloengular.easing',
+			'aloengular.roundProgress',
+			'aloengular.ee',
+		])
+	;
+
+})();
